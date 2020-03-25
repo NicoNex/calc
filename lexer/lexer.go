4a8a282802e908702c57e0b2e10a960ea946665b
Nicolò Santamaria <nicolo.santamaria@gmail.com>
2020-03-25 11:46:48 +0100

    Improved lexer [WIP]

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -3,16 +3,12 @@ package lexer
 import (
 	"os"
 	"fmt"
+	"errors"
 	"regexp"
 	"strconv"
 
 	"github.com/NicoNex/calc/ops"
 )
-
-type Token struct {
-	Type CalType
-	Value string
-}
 
 type CalType int
 const (
@@ -21,102 +17,134 @@ const (
   VARIABLE
 )
 
-func scanNum(data []byte) {
-
+type Token struct {
+	Type CalType
+	Value string
 }
 
-func lex(data []byte) ([]Token, error) {
-  var ret []Token
-  var buf []byte
-  var state CalType
-  var reop := regexp.MustCompile(`[\+\-\*\/]`)
-  var recns := regexp.MustCompile(`\d`)
-  var revar := regexp.MustCompile(`\w`)
-
-  for k, b := range data {
-    switch {
-    case
-    }
-  }
-}
-
-type newop func(a, b ops.Node) ops.Node
-
-const (
-	operator = `\+|\-|\*|\/|\=`
-	constant = `-?\d+(,\d+)*(\.\d+(e\d+)?)?`
-	variable = `\w*`
-)
-
-func die(msg ...interface{}) {
-	for _, m := range msg {
-		fmt.Println(m)
-	}
-	os.Exit(1)
-}
-
-func check(e error) {
-	if e != nil {
-		die("syntax error")
+func newToken(t CalType, v string) Token {
+	return Token{
+		t,
+		v,
 	}
 }
 
-func split(s string) []string {
-	re := regexp.MustCompile(`(-?\d+(,\d+)*(\.\d+(e\d+)?)?|\+|\-|\*|\/|\=|\w*)`)
-	return re.FindAllString(s, -1)
-}
+func scan(data []byte, re *regexp.Regexp) string {
+	var i int
 
-func populateAst(toks []string, node *ops.Node, i int) ops.Node {
-	return nil
-}
-
-func parseOperator(operator string) newop {
-	switch operator {
-	case "+":
-		return ops.NewPlus
-	case "-":
-		return ops.NewMinus
-	case "*":
-		return ops.NewTimes
-	case "/":
-		return ops.NewDivide
-
-	default:
-		die("error: unknown operator")
+	for re.Match(data[i]) && i < len(data) {
+		i++
 	}
 
-	return nil
+	return string(data[:i+1])
 }
 
-func Parse(s string) ops.Node {
-	var tmp ops.Node
-	var tree ops.Node
-	var tokens = split(s)
-	var rop = regexp.MustCompile(operator)
-	var rco = regexp.MustCompile(constant)
-	// var rva = regexp.MustCompile(variable)
+func Lex(data []byte) ([]Token, error) {
+	var ret []Token
+	var buf []byte
+	var state CalType
+	var reop = regexp.MustCompile(`[\+\-\*\/]`)
+	var recns = regexp.MustCompile(`\d`)
+	var revar = regexp.MustCompile(`\w`)
 
-	for k, t := range tokens {
+	for k, b := range data {
 		switch {
+		case reop.Match(b):
+			ret = append(ret, newToken(OPERATOR, string(b))
 
-		case rco.MatchString(t):
-			n, err := strconv.ParseFloat(t, 64)
-			check(err)
-			tmp = ops.NewConst(n)
+		case recns.Match(b):
+			val := scan(data[k:], recns)
+			ret = append(ret, newToken(CONSTANT, val))
 
-		case rop.MatchString(t):
-			n, err := strconv.ParseFloat(tokens[k+1], 64)
-			check(err)
-			newOperator := parseOperator(t)
+		case revar.Match(b):
+			val := scan(data[k:], revar)
+			ret = append(ret, newToken(VARIABLE, val))
 
-			if tree != nil {
-				tree = newOperator(tree, ops.NewConst(n))
-			} else {
-				tree = newOperator(tmp, ops.NewConst(n))
-			}
+		default:
+			return []Token{}, errors.New("invalid character")
 		}
 	}
 
-
-	return tree
+	return ret
 }
+
+// type newop func(a, b ops.Node) ops.Node
+
+// const (
+// 	operator = `\+|\-|\*|\/|\=`
+// 	constant = `-?\d+(,\d+)*(\.\d+(e\d+)?)?`
+// 	variable = `\w*`
+// )
+
+// func die(msg ...interface{}) {
+// 	for _, m := range msg {
+// 		fmt.Println(m)
+// 	}
+// 	os.Exit(1)
+// }
+
+// func check(e error) {
+// 	if e != nil {
+// 		die("syntax error")
+// 	}
+// }
+
+// func split(s string) []string {
+// 	re := regexp.MustCompile(`(-?\d+(,\d+)*(\.\d+(e\d+)?)?|\+|\-|\*|\/|\=|\w*)`)
+// 	return re.FindAllString(s, -1)
+// }
+
+// func populateAst(toks []string, node *ops.Node, i int) ops.Node {
+// 	return nil
+// }
+
+// func parseOperator(operator string) newop {
+// 	switch operator {
+// 	case "+":
+// 		return ops.NewPlus
+// 	case "-":
+// 		return ops.NewMinus
+// 	case "*":
+// 		return ops.NewTimes
+// 	case "/":
+// 		return ops.NewDivide
+
+// 	default:
+// 		die("error: unknown operator")
+// 	}
+
+// 	return nil
+// }
+
+// func Parse(s string) ops.Node {
+// 	var tmp ops.Node
+// 	var tree ops.Node
+// 	var tokens = split(s)
+// 	var rop = regexp.MustCompile(operator)
+// 	var rco = regexp.MustCompile(constant)
+// 	// var rva = regexp.MustCompile(variable)
+
+// 	for k, t := range tokens {
+// 		switch {
+
+// 		case rco.MatchString(t):
+// 			n, err := strconv.ParseFloat(t, 64)
+// 			check(err)
+// 			tmp = ops.NewConst(n)
+
+// 		case rop.MatchString(t):
+// 			n, err := strconv.ParseFloat(tokens[k+1], 64)
+// 			check(err)
+// 			newOperator := parseOperator(t)
+
+// 			if tree != nil {
+// 				tree = newOperator(tree, ops.NewConst(n))
+// 			} else {
+// 				tree = newOperator(tmp, ops.NewConst(n))
+// 			}
+// 		}
+// 	}
+
+
+// 	return tree
+// }
